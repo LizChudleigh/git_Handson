0549987e4ba6286ad4b922d1330fa268b4925ff3
Liz Chudleigh <emchudleigh@gmail.com>
2024-01-31 18:22:27 +0100

    Added line of code to Deal with U and T error

--- seqClass.py
+++ seqClass.py
@@ -13,6 +13,9 @@ if len(sys.argv) == 1:
 args = parser.parse_args()
 
 args.seq = args.seq.upper()                 # Note we just added this line
+
+if 'T' in args.seq and 'U' in args.seq:
+    print('The sequence is not DNA nor RNA (both T and U present)')
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
         print ('The sequence is DNA')

ed6ed69bf5510d01894628927b6a4d93e8826e15
LizChudleigh <158313717+LizChudleigh@users.noreply.github.com>
2024-02-18 18:54:36 +0100

    Update seqClass.py
    Moved parser.add_argument("-m", "--motif", type=str, required=False, help="Motif") line up and adjusted message printing for when T and U are present

--- seqClass.py
+++ seqClass.py
@@ -5,6 +5,7 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 
 if len(sys.argv) == 1:
     parser.print_help()
@@ -16,7 +17,7 @@ args.seq = args.seq.upper()                 # Note we just added this line
 
 if 'T' in args.seq and 'U' in args.seq:
     print('The sequence is not DNA nor RNA (both T and U present)')
-if re.search('^[ACGTU]+$', args.seq):
+elif re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
         print ('The sequence is DNA')
     elif re.search('U', args.seq):
@@ -34,4 +35,4 @@ if args.motif:
     else:
         print("This is NOT FOUND")
 
-parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
+
